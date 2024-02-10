45d71b875be47c298ba4e658031e82803a415f2d
Gray Liang <gray.liang@isovalent.com>
2024-02-11 02:09:17 +0800

    Add --all-kmods / -a

--- target/btf.go
+++ target/btf.go
@@ -1,19 +1,45 @@
 package target
 
 import (
-	"log"
+	"fmt"
+	"os"
 
 	"github.com/cilium/ebpf/btf"
 )
 
-var btfSpec *btf.Spec
+var btfSpecs []*btf.Spec
 
-func init() {
-	var err error
-	btfSpec, err = btf.LoadKernelSpec()
+func initBtfSpecs(allKmods bool) (err error) {
+	btfSpec, err := btf.LoadKernelSpec()
 	if err != nil {
-		log.Fatalf("failed to load kernel BTF: %+v\n", err)
+		return fmt.Errorf("failed to load kernel BTF: %v", err)
 	}
+	btfSpecs = append(btfSpecs, btfSpec)
+
+	if allKmods {
+		files, err := os.ReadDir("/sys/kernel/btf")
+		if err != nil {
+			return fmt.Errorf("failed to read BTF: %v", err)
+		}
+
+		for _, file := range files {
+			if file.IsDir() || file.Name() == "vmlinux" {
+				continue
+			}
+			f, err := os.Open("/sys/kernel/btf/" + file.Name())
+			if err != nil {
+				return fmt.Errorf("failed to open BTF: %v", err)
+			}
+			defer f.Close()
+
+			modSpec, err := btf.LoadSplitSpecFromReader(f, btfSpec)
+			if err != nil {
+				return fmt.Errorf("failed to load BTF: %v", err)
+			}
+			btfSpecs = append(btfSpecs, modSpec)
+		}
+	}
+	return
 }
 
 func getParamMap(symbols []string) map[string][]string {
@@ -23,45 +49,50 @@ func getParamMap(symbols []string) map[string][]string {
 	}
 
 	paramMap := map[string][]string{}
-	iter := btfSpec.Iterate()
-	for iter.Next() {
-		typ := iter.Type
-		fn, ok := typ.(*btf.Func)
-		if !ok {
-			continue
-		}
+	for _, spec := range btfSpecs {
+		iter := spec.Iterate()
+		for iter.Next() {
+			typ := iter.Type
+			fn, ok := typ.(*btf.Func)
+			if !ok {
+				continue
+			}
 
-		fnName := string(fn.Name)
-		if _, ok := symbolSet[fnName]; !ok {
-			continue
-		}
+			fnName := string(fn.Name)
+			if _, ok := symbolSet[fnName]; !ok {
+				continue
+			}
 
-		fnProto := fn.Type.(*btf.FuncProto)
-		for _, param := range fnProto.Params {
-			paramMap[fnName] = append(paramMap[fnName], string(param.Name))
+			fnProto := fn.Type.(*btf.FuncProto)
+			for _, param := range fnProto.Params {
+				paramMap[fnName] = append(paramMap[fnName], string(param.Name))
+			}
 		}
 	}
+
 	return paramMap
 }
 
 func searchSymbolsByRetval(paramType string) (symbols []string, err error) {
-	iter := btfSpec.Iterate()
-	for iter.Next() {
-		typ := iter.Type
-		fn, ok := typ.(*btf.Func)
-		if !ok {
-			continue
-		}
+	for _, spec := range btfSpecs {
+		iter := spec.Iterate()
+		for iter.Next() {
+			typ := iter.Type
+			fn, ok := typ.(*btf.Func)
+			if !ok {
+				continue
+			}
 
-		fnName := string(fn.Name)
+			fnName := string(fn.Name)
 
-		fnProto := fn.Type.(*btf.FuncProto)
-		if ptr, ok := fnProto.Return.(*btf.Pointer); ok {
-			if strct, ok := ptr.Target.(*btf.Struct); ok {
-				if strct.Name == paramType {
-					name := fnName
-					symbols = append(symbols, name)
-					continue
+			fnProto := fn.Type.(*btf.FuncProto)
+			if ptr, ok := fnProto.Return.(*btf.Pointer); ok {
+				if strct, ok := ptr.Target.(*btf.Struct); ok {
+					if strct.Name == paramType {
+						name := fnName
+						symbols = append(symbols, name)
+						continue
+					}
 				}
 			}
 		}
@@ -72,29 +103,31 @@ func searchSymbolsByRetval(paramType string) (symbols []string, err error) {
 func searchSymbolsByParam(paramType string) (pos2symbols map[int][]string, err error) {
 	pos2symbols = map[int][]string{}
 
-	iter := btfSpec.Iterate()
-	for iter.Next() {
-		typ := iter.Type
-		fn, ok := typ.(*btf.Func)
-		if !ok {
-			continue
-		}
+	for _, spec := range btfSpecs {
+		iter := spec.Iterate()
+		for iter.Next() {
+			typ := iter.Type
+			fn, ok := typ.(*btf.Func)
+			if !ok {
+				continue
+			}
 
-		fnName := string(fn.Name)
+			fnName := string(fn.Name)
 
-		fnProto := fn.Type.(*btf.FuncProto)
-		i := 0
-		for _, p := range fnProto.Params {
-			if ptr, ok := p.Type.(*btf.Pointer); ok {
-				if strct, ok := ptr.Target.(*btf.Struct); ok {
-					if strct.Name == paramType && i < 5 {
-						name := fnName
-						pos2symbols[i] = append(pos2symbols[i], name)
-						continue
+			fnProto := fn.Type.(*btf.FuncProto)
+			i := 0
+			for _, p := range fnProto.Params {
+				if ptr, ok := p.Type.(*btf.Pointer); ok {
+					if strct, ok := ptr.Target.(*btf.Struct); ok {
+						if strct.Name == paramType && i < 5 {
+							name := fnName
+							pos2symbols[i] = append(pos2symbols[i], name)
+							continue
+						}
 					}
 				}
+				i++
 			}
-			i++
 		}
 	}
 	return
--- target/target.go
+++ target/target.go
@@ -18,18 +18,27 @@ type Block interface {
 	Begins() []string
 }
 
-func New(target string, paramMap bool) (targetObj Target, err error) {
+type Options struct {
+	ParamMap bool
+	AllKmods bool
+}
+
+func New(target string, options *Options) (targetObj Target, err error) {
+	if err = initBtfSpecs(options.AllKmods); err != nil {
+		return
+	}
+
 	parts := strings.Split(target, ":")
 	if len(parts) != 2 {
 		return nil, fmt.Errorf("invalid target: %s", target)
 	}
 	switch parts[0] {
 	case "has_param":
-		targetObj = newTargetHasParam(parts[1], paramMap)
+		targetObj = newTargetHasParam(parts[1], options.ParamMap)
 	case "has_retval":
-		targetObj = newTargetHasRetval(parts[1], paramMap)
+		targetObj = newTargetHasRetval(parts[1], options.ParamMap)
 	case "from_file":
-		targetObj = newTargetFromFile(parts[1], paramMap)
+		targetObj = newTargetFromFile(parts[1], options.ParamMap)
 	default:
 		return nil, fmt.Errorf("unknown target: %s", parts[0])
 	}
